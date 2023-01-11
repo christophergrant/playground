dd9498d5cb9f2bdf0b4b4b2361b4cc991b27a349
toph <christopher.grant@protonmail.com>
2023-01-10 19:36:09 -0800

    init commit

---
+++ playground/main.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+
+def add(a, b):
+    return a + b
---
+++ tests/main_test.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import playground.main as pm
+
+
+def test_add():
+    assert pm.add(1, 2) == 3
